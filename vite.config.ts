d25e0ee143d87267ce43e58b576d01dd46fcab96
uab lauj <uablauJ76681809@gmail.com>
2025-06-20 14:28:23 +0700

    updata

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/ssr-react-seo/",
   plugins: [react(),tailwindcss()],
   ssr: {
     noExternal: ['react-router-dom', 'react-helmet-async'], // react-router-dom is included in the SSR bundle to ensure that it is handled correctly during server-side rendering. React Router DOM uses some browser-specific APIs like window or history that don't exist in Node.js environments. By bundling it, Vite ensures that these APIs are correctly polyfilled or handled during SSR.
@@ -11,4 +12,5 @@ export default defineConfig({
   optimizeDeps: {
     include: ['react-router-dom', 'react-helmet-async']
   },
+
 })
